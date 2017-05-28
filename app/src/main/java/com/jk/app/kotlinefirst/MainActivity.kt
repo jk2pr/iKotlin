b891a90d328c2df4237d80a85e4bfbf685d08b9f
jitendraprajapati <jk2pr@yahoo.co.in>
2017-05-28 18:19:25 +0530

    Initial commit

---
+++ app/src/main/java/com/jk/app/kotlinefirst/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.jk.app.kotlinefirst
+
+import kotlinx.android.synthetic.main.activity_main.*
+import android.app.Activity
+import android.os.Bundle
+import android.support.design.widget.FloatingActionButton
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        textview.text = "Hi!"
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id = item.itemId
+
+        if (id == R.id.action_settings) {
+            return true
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+}
