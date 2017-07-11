807556158abef7cf6f4f0616d23065d4a3d04a41
jitendraprajapati <jk2pr@yahoo.co.in>
2017-07-11 18:57:27 +0530

    added button click

--- app/src/main/java/com/jk/app/kotlinefirst/MainActivity.kt
+++ app/src/main/java/com/jk/app/kotlinefirst/MainActivity.kt
@@ -1,24 +1,35 @@
 package com.jk.app.kotlinefirst
 
+import android.app.Activity
 import android.os.Bundle
 import android.os.PersistableBundle
-import android.support.v7.app.AppCompatActivity
-import android.widget.Button
 import kotlinx.android.synthetic.main.activity_main.*
 import org.jetbrains.anko.toast
 
 /**
  * Created by jitendraprajapati on 28/05/17.
  */
-public class MainActivity: AppCompatActivity(){
+public class MainActivity: Activity(){
 
     override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
         super.onCreate(savedInstanceState, persistentState)
-        setContentView(R.layout.activity_main)
-        textview.text=getString(R.string.hi_text)
-        button.setOnClickListener({
+        toast("OnCreate Clicked")
+
+      //  textview.text=getString(R.string.hi_text)
+      /*  button.setOnClickListener({
             toast("button Clicked")
-        })
+        })t*/
+
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        setContentView(R.layout.activity_main)
+          button.setOnClickListener({
+           toast("button Clicked")
+       })
+
 
     }
 }
